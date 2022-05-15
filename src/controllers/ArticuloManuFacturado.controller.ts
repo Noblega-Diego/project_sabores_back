a29389cec6278a79d63b18cb7146cea8e6876ff8
Diego Noblega <diegomendoza20015@gmail.com>
2022-05-15 12:08:44 -0300

    se creo dto articulo

--- src/controllers/ArticuloManuFacturado.controller.ts
+++ src/controllers/ArticuloManuFacturado.controller.ts
@@ -4,8 +4,15 @@ const ModelArticuloManufacturado = require('./../database/models').ArticuloManuf
 const ModelRubroGeneral = require('./../database/models').RubroGeneral
 const ModelPrecioArticulo = require('./../database/models').PrecioArticuloManufacturado
 
-
+interface Articulo{
+    nombre:string,
+    precio:number
+}
 export const getAll = async (req:Request, res:Response) => {
     let articulos = await ModelArticuloManufacturado.findAll({include: [ { model:ModelRubroGeneral }, { model:ModelPrecioArticulo} ]})
-    res.send(articulos);
+    res.send(articulos.map((art:any)=>{let a:Articulo = {
+        nombre:art.denominacion,
+        precio:art.PrecioArticuloManufacturados[0].precioVenta
+    }; return a}));
+    
 }
